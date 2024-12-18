79e8caa2586da293c78a674f7153b2aa4e2b3a9e
alisalali <alis.alali@hotmail.com>
2024-12-19 01:56:26 +0300

    19-returning-responses

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,8 +1,11 @@
-import { useLoaderData } from "react-router";
+import { useLoaderData } from "react-router-dom";
+
 import EventsList from "../components/EventsList";
 
 function EventsPage() {
-  const events = useLoaderData();
+  const data = useLoaderData();
+  const events = data.events;
+
   return <EventsList events={events} />;
 }
 
@@ -12,9 +15,8 @@ export async function loader() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    //
+    // ...
   } else {
-    const resData = await response.json();
-    return resData.events;
+    return response;
   }
 }
