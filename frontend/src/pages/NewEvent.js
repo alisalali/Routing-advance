d23ff46985316b367ed98bdce3ecf1c0bd5ceb14
alisalali <alis.alali@hotmail.com>
2024-12-19 22:21:17 +0300

    24-action-data-submission

--- frontend/src/pages/NewEvent.js
+++ frontend/src/pages/NewEvent.js
@@ -1,5 +1,34 @@
 import React from "react";
+import EventForm from "../components/EventForm";
+import { redirect } from "react-router";
 
 export default function NewEventPage() {
-  return <div>NewEventPage</div>;
+  return <EventForm />;
+}
+
+export async function action({ request, params }) {
+  const data = await request.formData();
+
+  const eventData = {
+    title: data.get("title"),
+    image: data.get("image"),
+    date: data.get("date"),
+    description: data.get("description"),
+  };
+
+  const response = await fetch("http://localhost:8080/events/", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(eventData),
+  });
+
+  if (!response.ok) {
+    throw Response.json(
+      { message: "Could not create new event" },
+      { status: 500 }
+    );
+  }
+  return redirect("/events");
 }
