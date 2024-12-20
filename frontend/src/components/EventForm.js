e1b2652650341334457f463fa4a0f5b3107f23b1
alisalali <alis.alali@hotmail.com>
2024-12-20 13:57:33 +0300

    27-validation-returning-data

--- frontend/src/components/EventForm.js
+++ frontend/src/components/EventForm.js
@@ -1,10 +1,12 @@
-import { Form, useNavigate, useNavigation } from "react-router";
+import { Form, useActionData, useNavigate, useNavigation } from "react-router";
 
 import classes from "./EventForm.module.css";
 
 function EventForm({ method, event }) {
+  const data = useActionData();
   const navigate = useNavigate();
   const navigation = useNavigation();
+
   function cancelHandler() {
     navigate("..");
   }
@@ -13,6 +15,13 @@ function EventForm({ method, event }) {
 
   return (
     <Form method="post" className={classes.form}>
+      {data && data.errors && (
+        <ul>
+          {Object.values(data.errors).map((err) => (
+            <li key={err}>{err}</li>
+          ))}
+        </ul>
+      )}
       <p>
         <label htmlFor="title">Title</label>
         <input
