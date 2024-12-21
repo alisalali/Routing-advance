b38b71457eb3f7da0db67d260e4230f393a59396
alisalali <alis.alali@hotmail.com>
2024-12-21 20:11:30 +0300

    31-defer-introduction

--- frontend/src/pages/Events.js
+++ frontend/src/pages/Events.js
@@ -1,6 +1,7 @@
-import { useLoaderData } from "react-router";
+import { Await, useLoaderData } from "react-router";
 
 import EventsList from "../components/EventsList";
+import { Suspense } from "react";
 
 function EventsPage() {
   const data = useLoaderData();
@@ -10,29 +11,57 @@ function EventsPage() {
   // }
   const events = data.events;
 
-  return <EventsList events={events} />;
+  return (
+    <Suspense fallback={<p style={{ textAlign: "center" }}>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
-    // return { isError: true, message: 'Could not fetch events.' };
-    throw Response.json(
-      { message: "Could not fetch events." },
-      {
-        status: 500,
-      }
-    );
-    // throw json(
-    //   { message: 'Could not fetch events.' },
-    //   {
-    //     status: 500,
-    //   }
-    // );
-  } else {
-    return response;
+    throw new Error("Could not fetch events.");
+  }
+
+  const resData = await response.json();
+  return resData.events;
+}
+
+export function loader() {
+  try {
+    const events = loadEvents();
+    return { events };
+  } catch (error) {
+    throw new Response(JSON.stringify({ message: error.message }), {
+      status: 500,
+    });
   }
 }
+
+// export async function loader() {
+//   const response = await fetch("http://localhost:8080/events");
+
+//   if (!response.ok) {
+//     // return { isError: true, message: 'Could not fetch events.' };
+//     throw Response.json(
+//       { message: "Could not fetch events." },
+//       {
+//         status: 500,
+//       }
+//     );
+//     // throw json(
+//     //   { message: 'Could not fetch events.' },
+//     //   {
+//     //     status: 500,
+//     //   }
+//     // );
+//   } else {
+//     return response;
+//   }
+// }
